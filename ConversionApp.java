41b0c04f87234898b0db275a92da13de984d3514
gd-ogra <gauru.dogra@gmail.com>
2020-07-04 22:35:43 +0100

    second stage completed

--- ConversionApp.java
+++ ConversionApp.java
@@ -1,70 +1,182 @@
 package converter;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class ConversionApp {
+class ConversionApp {
+    private final String input;
+    private String json = null;
+    private String xml = null;
 
-    public String convert(String inputString) throws Exception {
-        inputString = inputString.trim();
-        IConvert converter;
-
-        if (inputString.charAt(0) == '<') {
-            converter = this::xmlToJson;
-
-        } else if (inputString.charAt(0) == '{') {
-            converter = this::jsonToXml;
-        } else {
-            throw new Exception("Unable to parse input String");
-        }
-
-        return converter.convert(inputString);
+    public ConversionApp(String input) {
+        this.input = input.trim();
+        parse(input);
     }
 
-    private String jsonToXml(String input) {
+    public boolean sourceIsXml() {
+        return input.startsWith("<");
+    }
 
-        Pattern pattern = Pattern.compile("\".+?\"");
+    public String getJson() {
+        return json;
+    }
+
+    public String getXml() {
+        return xml;
+    }
+
+    private void parse(String input) {
+        if (sourceIsXml()) {
+            xml = input;
+            json = parseToJson(input);
+        } else {
+            json = input;
+            xml = parseToXml(input);
+        }
+    }
+
+    private String parseToXml(String input) {
+        String element = getElementFromJson(input);
+        String elementValue = getElementValueFromJson(input, element);
+        Map<String, String> attributes = getAttributesFromJson(input);
+        return generateXml(element, elementValue, attributes);
+    }
+
+    private String generateXml(String element, String elementValue, Map<String, String> attributes) {
+        if (attributes.isEmpty()) {
+            return String.format("<%s>%s</%s>", element, elementValue, element);
+        }
+
+        StringBuilder sb = new StringBuilder("<");
+        sb.append(element).append(" ");
+        for (String attribute : attributes.keySet()) {
+            sb.append(attribute).append(" = ").append("\"").append(attributes.get(attribute)).append("\" ");
+        }
+        if (elementValue == null) {
+            sb.append("/>");
+        } else {
+            sb.append(">").append(elementValue).append("</").append(element).append(">");
+        }
+        return sb.toString();
+    }
+
+    private Map<String, String> getAttributesFromJson(String input) {
+        String[] array = input.split("[:,]");
+        Map<String, String> attributes = new HashMap<>();
+
+        if (array.length == 2) {
+            return attributes;
+        }
+
+
+        for (int i = 0; i < array.length; i++) {
+            String str = array[i].replaceAll("[\"{}]", "").trim();
+            if (str.startsWith("@")) {
+                attributes.put(str.substring(1), array[i + 1].replaceAll("[\"{}]", "").trim());
+            }
+        }
+        return attributes;
+    }
+
+    private String getElementValueFromJson(String input, String element) {
+        String[] array = input.split("[:,]");
+        if (array.length == 2) {
+            return array[1].replaceAll("[\"}]", "").trim();
+        }
+
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i].contains("#" + element)) {
+                String returnVal = array[i + 1].replaceAll("[\"}]", "").trim();
+                return returnVal.equals("null") ? null : returnVal;
+            }
+        }
+        return null;
+    }
+
+    private String getElementFromJson(String input) {
+        input = input.replaceAll("\\s+", "");
+        Pattern elementPattern = Pattern.compile("\\{\".+?\"");
+        Matcher elementMatcher = elementPattern.matcher(input);
+        if (elementMatcher.find()) {
+            String element = input.substring(elementMatcher.start(), elementMatcher.end());
+            return element.replaceAll("[\"{]", "");
+        }
+        return null;
+    }
+
+    private String parseToJson(String input) {
+        String element = getElementFromXml(input);
+        String elementValue = getElementValueFromXml(input, element);
+        Map<String, String> attributes = getAttributesFromXml(input, element);
+        return generateJson(element, elementValue, attributes);
+    }
+
+    private String generateJson(String element, String elementValue, Map<String, String> attributes) {
+        StringBuilder sb = new StringBuilder("{ \"");
+        sb.append(element).append("\" : ");
+        if (attributes.isEmpty()) {
+            sb.append("\"").append(elementValue).append("\" }");
+        } else {
+            sb.append("{ ");
+            for (String attribute : attributes.keySet()) {
+                sb.append("\"@").append(attribute).append("\" : ").append("\"").append(attributes.get(attribute))
+                        .append("\", ");
+            }
+            if (elementValue == null) {
+                sb.append("\"#").append(element).append("\" : ").append(elementValue).append(" } }");
+            } else {
+                sb.append("\"#").append(element).append("\" : \"").append(elementValue)
+                        .append("\"").append(" } }");
+            }
+        }
+        return sb.toString();
+    }
+
+    private Map<String, String> getAttributesFromXml(String input, String element) {
+        Map<String, String> attributes = new HashMap<>();
+
+        Pattern pattern = Pattern.compile("<" + element + " " + "(.+?)" + "[>/]");
         Matcher matcher = pattern.matcher(input);
-        String key = null;
-        String value = null;
-
+        String attributesString = null;
         if (matcher.find()) {
-            key = input.substring(matcher.start() + 1, matcher.end() - 1);
+            attributesString = matcher.group(1);
+        }
+        if (attributesString == null) {
+            return attributes; // empty return
         }
 
-        if (matcher.find()) {
-            value = input.substring(matcher.start() + 1, matcher.end() - 1);
+        attributesString = attributesString.replaceAll("[\"=]", "");
+        String[] attributesArray = attributesString.split("\\s+");
+        for (int i = 0; i < attributesArray.length - 1; i = i + 2) {
+            attributes.put(attributesArray[i], attributesArray[i + 1]);
         }
-
-        if (value == null && key != null) {
-            return "<" + key + "/>";
-        } else {
-
-            return "<" + key + ">" + value + "</" + key + ">";
-        }
+        return attributes;
     }
 
-    private String xmlToJson(String input) {
-
-        Pattern pattern = Pattern.compile("<.*?>");
-        Matcher firstMatcher = pattern.matcher(input);
-        String key = null;
-        if (firstMatcher.find()) {
-            key = input.substring(firstMatcher.start() + 1, firstMatcher.end() - 1);
-        }
-        pattern = Pattern.compile("</.*>");
-        Matcher secondMatcher = pattern.matcher(input);
-        String value = null;
-        if (secondMatcher.find()) {
-            value = input.substring(firstMatcher.end(), secondMatcher.start());
+    private String getElementValueFromXml(String input, String element) {
+        Pattern pattern = Pattern.compile("<" + element + ">(.+)</" + element + ">");
+        Matcher matcher = pattern.matcher(input);
+        if (matcher.matches()) {
+            return matcher.group(1);
         }
 
-        if (value == null && key != null) {
-            key = key.substring(0, key.length() - 1);
-            return "{\"" + key + "\": " + null + " }";
-        } else {
-            return "{\"" + key + "\":\"" + value + "\"}";
+        pattern = Pattern.compile("<" + element + " " + ".+>(.+)</" + element + ">");
+        matcher = pattern.matcher(input);
+        if (matcher.matches()) {
+            return matcher.group(1);
         }
+        return null;
+    }
 
+    private String getElementFromXml(String input) {
+        Pattern elementPattern = Pattern.compile("<.+?[\\s+>]");
+        Matcher elementMatcher = elementPattern.matcher(input);
+        if (elementMatcher.find()) {
+            return input.substring(1, elementMatcher.end() - 1);
+        }
+        return null;
     }
 }
--- Main.java
+++ Main.java
@@ -1,14 +1,22 @@
 package converter;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
-        ConversionApp app = new ConversionApp();
-        Scanner scanner = new Scanner(System.in);
-
-        String output = app.convert(scanner.nextLine());
+    public static void main(String[] args) throws IOException {
+        String input = readInput();
+        ConversionApp app = new ConversionApp(input);
+        String output = app.sourceIsXml() ? app.getJson() : app.getXml();
         System.out.println(output);
     }
+
+    private static String readInput() throws IOException {
+//        final Scanner scanner = new Scanner(System.in);
+        return Files.readString(Paths.get("test.txt"));
+    }
 }
+
